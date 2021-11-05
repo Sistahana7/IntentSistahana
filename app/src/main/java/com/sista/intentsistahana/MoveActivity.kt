c1ba67db7c46db14cb6c501d43b138f56b08132e
Sistahana7 <sistahana4@gmail.com>
2021-11-05 18:37:31 +0700

    Latihan Intent dengan Android Studio
    Latihan Intent dengan bahasa Kotlin di Android Studio

---
+++ app/src/main/java/com/sista/intentsistahana/MoveActivity.kt
@@ -0,0 +1,19 @@
+package com.sista.intentsistahana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MoveActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(){
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/com/sista/intentsistahana/MoveWithDataActivity.kt
@@ -0,0 +1,34 @@
+package com.sista.intentsistahana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MoveWithDataActivity : AppCompatActivity() {
+    private lateinit var tvDataReceived: TextView
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_data)
+
+        tvDataReceived = findViewById(R.id.tv_data_received)
+        DataReceived()
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(){
+            super.onBackPressed();
+        }
+    }
+
+    private fun DataReceived() {
+        val bundle = intent.extras
+        val nama = bundle?.getString("Nama")
+        val alamat = bundle?.getString("Alamat")
+        val nim = bundle?.getString("NIM")
+
+        tvDataReceived.text = nama + " " + ", " + alamat + " " + ", " +nim
+    }
+}
